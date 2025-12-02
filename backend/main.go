33323b47d4f208ae343cca5062c311683a641280
OrMichal <misaorsah5@gmail.com>
2025-12-02 19:30:32 +0100

    backkend render

--- backend/main.go
+++ backend/main.go
@@ -62,8 +62,9 @@ func main() {
 
 	port := os.Getenv("PORT")
 	if port == "" {
+		log.Println("Could not get PORT from environment vars")
 		port = "8080"
 	}
 
-	log.Fatal(http.ListenAndServe(":" + port, handler))
+	log.Fatal(http.ListenAndServe(":"+port, handler))
 }
